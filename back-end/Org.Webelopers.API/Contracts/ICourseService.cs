811fbd76cca9d9358f4323e95df72784add87fba
Mircea Negrău <mircea.negrau1@gmail.com>
2022-03-27 18:59:57 +0300

    Added Course service

---
+++ back-end/Org.Webelopers.API/Contracts/ICourseService.cs
@@ -0,0 +1,14 @@
+﻿using Org.Webelopers.Api.Models.DbEntities;
+using System;
+using System.Collections.Generic;
+
+namespace Org.Webelopers.Api.Contracts
+{
+    public interface ICourseService
+    {
+        public void AddCourse(string name, short credits, Guid semesterId, Guid teacherId);
+        public void UpdateCourse(Guid courseId, string name, short? credits, Guid? semesterId, Guid? teacherId);
+        public void DeleteCourse(Guid courseId);
+        public List<Course> GetCoursesByTeacher(Guid teacherId);
+    }
+}
