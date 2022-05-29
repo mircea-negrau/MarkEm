c975304debefeff2dc1398dc9fe35e78296ef91a
PopEmanuel <emanuelpop757@yahoo.com>
2022-05-29 16:54:24 +0300

    changed grades representation

--- back-end/Org.Webelopers.API/Models/Persistence/Grades/SemesterCourseGrades.cs
+++ back-end/Org.Webelopers.API/Models/Persistence/Grades/SemesterCourseGrades.cs
@@ -1,13 +1,18 @@
 ﻿
 
 using Org.Webelopers.Api.Models.Persistence.Semester;
+using System;
 using System.Collections.Generic;
 
 namespace Org.Webelopers.Api.Models.Persistence.Grades
 {
     public class SemesterCourseGrades
     {
-        public SemesterDto Semester { get; set; }
+        public Guid Id { get; set; }
+        public short Value { get; set; }
+        public long YearStartDate { get; set; }
+        public long YearEndDate { get; set; }
+        public string SpecializationName { get; set; }
         public List<StudentCourseGrades> Courses { get; set; }
 
     }
